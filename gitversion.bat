dc78ac6e4020a57eb02130a569df2028e6459b9e
Joey Kraut <notjoeykrautokaymaybeitis@gmail.com>
2015-02-11 20:13:54 -0800

    Added the status define to gitversion.bat.

--- gitversion.bat
+++ gitversion.bat
@@ -1,8 +1,9 @@
 @echo off
 
-echo //This file simply contains the git hash of the code as set by gitversion.bat > ../src/GitVersion.h
-<nul (set/p tmp=#define GIT_VERSION ) >> ../src/GitVersion.h
-git log -1 --pretty="%%h" >> ../src/GitVersion.h
-
-
+echo //This file simply contains the git hash of the code as set by gitversion.bat > src/GitVersion.h
+<nul (set/p version=#define GIT_VERSION ) >> src/GitVersion.h
+git log -1 --pretty="%%h" >> src/GitVersion.h
+<nul (set/p status=#define STATUS ) >> src/GitVersion.h
+git status --short >> src/GitVersion.h
+pause
 exit
