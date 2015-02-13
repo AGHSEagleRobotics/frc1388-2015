55ecc0878c62988cc4f080baeb7ae90f356606ed
Joey Kraut <notjoeykrautokaymaybeitis@gmail.com>
2015-02-13 13:00:44 -0800

    Added the TAG define to gitversion.bat

--- gitversion.bat
+++ gitversion.bat
@@ -3,8 +3,11 @@ git log -1 --pretty="%%h" > git.tmp
 set /p VERSION=<git.tmp
 git status -z > git.tmp
 set /p STATUS=<git.tmp
+git tag --contains > git.tmp
+set /p TAG=<git.tmp
 del git.tmp
 echo //This file simply contains the git info of the code as set by gitversion.bat > src/GitVersion.h
 echo #define GIT_VERSION ^"%VERSION%^" >> src/GitVersion.h
-echo #define STATUS ^"%STATUS%^" >> src/GitVersion.h
+echo #define STATUS ^"%STATUS%^" >> src/GitVersion.h	
+echo #define TAG ^"%TAG%^" >> src/GitVersion.h
 exit
